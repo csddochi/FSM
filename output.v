7005e9be726a6dce9b45db2f20fb8889e9c33dce
RENESYS <hrs350x@gmail.com>
2016-07-12 15:39:37 -0700

    add other example .vcd file

--- output.v
+++ output.v
@@ -1,38 +1,38 @@
-module tb_and_test(reset, clk, i2, i1);
-	input reset, clk, i2, i1;
-	parameter s1 = 0, s2 = 500, s3 = 1000, s4 = 1500, s5 = 2000, s6 = 2300, s7 = 2800, s8 = 3000, s9 = 3300, s10 = 3500, s11 = 4000, s12 = 4500;
+module tb_and_test(reset, clk, i1, i2, i4, i3);
+	input reset, clk, i1, i2, i4, i3;
+	parameter s1 = 0, s2 = 200, s3 = 700, s4 = 900, s5 = 1000, s6 = 1300, s7 = 1800, s8 = 2300, s9 = 2800, s10 = 3100, s11 = 3400, s12 = 3600, s13 = 3800, s14 = 4100;
 	reg[1:0] state, nextState;
 
 	always @(posedge clk) begin
 		if(reset) begin
 			state <= s1;
-			i2 = 0, i1 = 0;
+			i1 = 0, i2 = 0, i4 = 0, i3 = 0;
 		end
 		else begin
 			state <= nextState;
 		end
 	end
 
-	always @(i2, i1) begin
+	always @(i1, i2, i4, i3) begin
 		case(state)
 		s1 : begin
-			if(i1 == 1) nextState <= s2;
+			if(i3 == 1) nextState <= s2;
 			else nextState <= s1;
 		end
 		s2 : begin
-			if(i2 == 1) nextState <= s3;
+			if(i1 == 1) nextState <= s3;
 			else nextState <= s1;
 		end
 		s3 : begin
-			if(i1 == 0) nextState <= s4;
+			if(i3 == 0) nextState <= s4;
 			else nextState <= s1;
 		end
 		s4 : begin
-			if(i2 == 0) nextState <= s5;
+			if(i4 == 1) nextState <= s5;
 			else nextState <= s1;
 		end
 		s5 : begin
-			if(i1 == 1) nextState <= s6;
+			if(i1 == 0) nextState <= s6;
 			else nextState <= s1;
 		end
 		s6 : begin
@@ -40,26 +40,34 @@ module tb_and_test(reset, clk, i2, i1);
 			else nextState <= s1;
 		end
 		s7 : begin
-			if(i1 == 0) nextState <= s8;
+			if(i1 == 1) nextState <= s8;
 			else nextState <= s1;
 		end
 		s8 : begin
-			if(i2 == 0) nextState <= s9;
+			if(i4 == 0) nextState <= s9;
 			else nextState <= s1;
 		end
 		s9 : begin
-			if(i2 == 1) nextState <= s10;
+			if(i3 == 1) nextState <= s10;
 			else nextState <= s1;
 		end
 		s10 : begin
-			if(i1 == 1) nextState <= s11;
+			if(i4 == 1) nextState <= s11;
 			else nextState <= s1;
 		end
 		s11 : begin
-			if(i1 == 0) nextState <= s12;
+			if(i2 == 0) nextState <= s12;
 			else nextState <= s1;
 		end
 		s12 : begin
+			if(i1 == 0) nextState <= s13;
+			else nextState <= s1;
+		end
+		s13 : begin
+			if(i3 == 0) nextState <= s14;
+			else nextState <= s1;
+		end
+		s14 : begin
 		end
 		endcase
 	end
